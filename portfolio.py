ba0506aa18dbc647835c1296e67ed589df206fcb
AJacobs15 <ajacobs15@uchicago.edu>
2017-06-17 17:14:49 -0500

    updates

--- portfolio.py
+++ portfolio.py
@@ -2,3 +2,5 @@ import twitter
 
 api = twitter.Api(consumer_key = 'KBwykgDKssvUi6a9g6AQouekf', consumer_secret='0J5hxQubZpq1D9oFGgKlF8nmejhlziYfnToOqzZI0iDKKKn8Wp', access_token_key='150235316-ACR5oR7s44sAuDokXNwZ8K8c4H4bSNwp5Exy1kvt', access_token_secret = 'zOGtr4iEAdbvfC2C3i3lakd2inx3UCI4COm1qaSyOwFdK')
 
+users = api.GetFriends()
+print([u.name for u in users])
